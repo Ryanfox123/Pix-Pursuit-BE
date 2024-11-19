a19f55130a028fecdb88cb0514d288cf9fdb86c1
Kyle-Wearing <kylewearing@icloud.com>
2024-11-19 10:04:16 +0000

    fix run seed imports

--- DB/seeds/run-seed.js
+++ DB/seeds/run-seed.js
@@ -1,5 +1,5 @@
-const seed = require(".db/seed");
-const db = require("..db/connection.js");
+const seed = require("./seed.js");
+const db = require("../connection.js");
 const devData = require("../data/devData/index.js");
 
 const runSeed = () => {
