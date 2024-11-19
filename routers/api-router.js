89c2f339a5ac9f1454388e87e854c6a0ac733d44
Kyle-Wearing <kylewearing@icloud.com>
2024-11-19 16:43:22 +0000

    setup endpoint.json file and test that /api gets it

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,6 +3,7 @@ const usersRouter = require("./users-router");
 const purusitsRouter = require("./pursuits-router");
 const pursuitsCompletedByUsersRouter = require("./pursuitsCompletedByUsers-router");
 const usersToCurrentPursuitRouter = require("./usersToCurrentPursuit-router");
+const endpointsJSON = require("../endpoints.json");
 
 apiRouter.use("/users", usersRouter);
 
@@ -13,7 +14,7 @@ apiRouter.use("/pursuitsCompletedByUsers", pursuitsCompletedByUsersRouter);
 apiRouter.use("/usersToCurrentPursuit", usersToCurrentPursuitRouter);
 
 apiRouter.get("/", (req, res) => {
-  res.status(200).send("All OK from /api");
+  res.status(200).send({ endpoints: endpointsJSON });
 });
 
 module.exports = apiRouter;
