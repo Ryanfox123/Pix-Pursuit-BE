f5d9b32f894c36297a6d1a3ccc0d56dde0bef9ad
Kyle-Wearing <kylewearing@icloud.com>
2024-11-20 11:43:41 +0000

    allow cross origin rescourse sharing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const apiRouter = require("./routers/api-router");
 const { customErrors, sqlErrors } = require("./error-handler");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
